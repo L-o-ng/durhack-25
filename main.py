743f2dedb286c11e3ead588efb7e1ba2769f73ba
Dylan <dylant7192@gmail.com>
2025-11-01 13:19:23 +0000

    boilerplate for pygame

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((1280, 720)).convert()
+
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            raise SystemExit
+
+    ### Logic Updates ###
+
+    screen.fill("purple")
+
+    ### Graphics Render ###
+
+    pygame.display.flip()
+    clock.tick(60)         
